30ca30348aea8f05de547c91a819603736ce1203
HawkstoNGriM <filip.omazic98@gmail.com>
2022-03-16 17:59:30 +0100

    Implemented VD for Joomla
    Implemented an option for versiondetection for Joomla

--- versionDetect-TODO.php
+++ versionDetect-TODO.php
@@ -9,21 +9,23 @@ function versionDetectorFunction($url, $cms){
         "Joomla"
     ];
 
-    echo $url . " has " . $cms . " ... Detecting version for it. <br/>";
-    
+    //echo $url . " has " . $cms . " ... Detecting version for it. <br/>";
+    echo "<br/>Detecting the version for CMS...</br>";
+
     $versionSource = "";
     foreach($systems as $s){
         if(str_contains(strtoupper($cms), strtoupper($s))){
-            echo $cms . " has CMS:" . $s . "<br/>";
+            //echo $cms . " has CMS:" . $s . "<br/>";
             $versionSource = $s;
+            //$cms was "Joomla " and now its "Joomla" - assigned the right one 
+            //according to the list
         }
         else{
         }
         
     }
     if($versionSource !== ""){
-        #echo "Checked the name, now its not 'Joomla_1' or whatever, its 'Joomla' - ready for testing.";
-    
+
         // TODO 
         // IMPLEMETN VERSION DETECTION FOR EACH CMS
 
@@ -41,11 +43,50 @@ function versionDetectorFunction($url, $cms){
         if($versionSource == $systems[1]){
             echo "<br/>";
             #System : Joomla
-            $response = file_get_contents($url);
-            if(str_contains(strtoupper($response),strtoupper($cms) )) {
-                echo "IT CONTAINS";
-                //TODO 
+
+            $options = ["/administrator/manifests/files/joomla.xml","/includes/version.php","/libraries/joomla/version.php","/libraries/cms/version/version.php","/", "/README.txt"];
+            
+            foreach($options as $option){
+                $url2 = $url . $option;
+                //echo "Trying :" . $url2 . "<br/>";
+                try{
+                    $response = file_get_contents($url2);
+                    $response = htmlentities($response);
+                    //the version for my /joom is 3.10.4 
+                    //echo $response;
+
+
+                    if(str_contains(strtoupper($response),strtoupper($versionSource) )) {    
+
+                        //check the xml if its it           RENAME TO "version" I PUT THIS TO TEST THE OTHER ONE
+                        if(str_contains(strtolower($response), "versionxxx")){
+                            //maybe its version=3.10.4
+                            //and maybe its <version>3.10.4</version>
+                            //echo $response;
+                            $response = html_entity_decode($response);
+                            if(str_contains($response,"</version")){
+                                $response = explode("</version>",$response);
+                                $versionDirty = explode("<version>", $response[0]);
+                                echo "<br> Version :<br/>" . $versionDirty[1] . "<hr/>"; 
+                            }
+
+
+                        }
+
+
+                        else if(str_contains(strtolower($response), "version history")){
+                            echo "TODO, also rename upper IF when done.";
+
+                            echo "";
+                        }
+
+                    }
+
+                } catch(Error $e){
+                    echo "<p hidden> " . $e . "</p>";
+                }
             }
+
 
 
         }
@@ -59,7 +100,7 @@ function versionDetectorFunction($url, $cms){
 
 }
 
-versionDetectorFunction("http://localhost/joom","Joomla");
+versionDetectorFunction("http://localhost/joom","Joomla ");
 
 
     

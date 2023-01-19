4d23bb704ba0be581e947a06e82d510d633f4c7d
HawkstoNGriM <40773153+HawkstoNGriM@users.noreply.github.com>
2023-01-19 19:45:46 +0100

    Fixed the URL
    Thanks to skiddie0057

--- Updater.sh
+++ Updater.sh
@@ -13,8 +13,8 @@ wget https://cve.mitre.org/data/downloads/allitems.csv -O allCVEs2022.csv
 #Windows : Invoke-WebRequest -Uri "https://cve.mitre.org/data/downloads/allitems.csv" -OutFile "allCVEs2022.csv"
 #i didnt test this windows command but it should work fine (the only thing that might be odd is the Outfile directory
 #Like I'd go: ./allCVEs2022.csv but i think this should work fine if the script /command is ran from the folder
+wget https://gitlab.com/exploit-database/exploitdb/-/raw/main/files_exploits.csv?inline=false files_exploits.csv
 
-wget https://raw.githubusercontent.com/offensive-security/exploitdb/master/files_exploits.csv files_exploits.csv
 #Windows Invoke-WebRequest -Uri "https://raw.githubusercontent.com/offensive-security/exploitdb/master/files_exploits.csv" -OutFile "files_exploits.csv"
 
 wget https://www.cisa.gov/sites/default/files/csv/known_exploited_vulnerabilities.csv known_exploited_vulnerabilities.csv
